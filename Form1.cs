83ae2b3167de5ce052c3f61141ad9996379fad32
Mah0rka <131210581+Mah0rka@users.noreply.github.com>
2024-04-01 23:21:04 +0300

    v2

--- Form1.cs
+++ Form1.cs
@@ -2,34 +2,34 @@ namespace oop17
 {
     public partial class OOP17 : Form
     {
-        Hyperbola hyper = new Hyperbola();
+        Hyperbola hyper = new Hyperbola(); // створення обєкту класу Hyperbola
 
-        Ellipse elips = new Ellipse(0, 0, 3, 5);
+        Ellipse elips = new Ellipse(0, 0, 3, 5); // створення обєкту класу Ellipse (ініціалізація через конструктор)
 
         public OOP17()
         {
             InitializeComponent();
         }
 
-        private void button_hyperbola_Click(object sender, EventArgs e)
+        private void button_hyperbola_Click(object sender, EventArgs e)//обчислення y для гіперболи
         {
             try
             {
                 label_hyperbola.Text = hyper.Calculate(Convert.ToDouble(textBox_hyperbola.Text)).ToString();
             }
-            catch (FormatException)
+            catch (FormatException)//захист
             {
                 MessageBox.Show("Тільки числа");
             }
         }
 
-        private void button_elips_Click(object sender, EventArgs e)
+        private void button_elips_Click(object sender, EventArgs e)//обчислення y для еліпса
         {
             try
             {
                 label_elipse.Text = elips.Calculate(Convert.ToDouble(textBox_elipse.Text)).ToString();
             }
-            catch (FormatException)
+            catch (FormatException)//захист
             {
                 MessageBox.Show("Тільки числа");
             }
